b2231436bd28622e55a453cf0daee8f6ca0d445c
Jenny Heath <jennifer.w.heath@gmail.com>
2018-09-27 12:53:32 -0700

    close modal on overlay click

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -7,8 +7,7 @@ import styled from 'styled-components';
 Modal.setAppElement('#root');
 
 const modalStyle = {
-  overlay: {},
-  content : {
+  content: {
     margin: '0 auto',
     width: '50%',
     height: '70%'
@@ -18,9 +17,32 @@ const modalStyle = {
 class CustomModal extends Component {
   constructor(props) {
     super(props);
+    this.handleOverlayClick = this.handleOverlayClick.bind(this);
     this.onEscape = this.onEscape.bind(this);
     this.navigateHome = this.navigateHome.bind(this);
-    // this.state = {overlayRef: null};
+    this.state = {overlayRef: null};
+  }
+
+  componentDidMount() {
+    document.addEventListener('keydown', this.onEscape, false);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.onEscape, false);
+  }
+
+  componentDidUpdate() {
+    const {overlayRef} = this.state;
+    if (overlayRef) {
+      overlayRef.addEventListener('click', this.handleOverlayClick);
+    }
+  }
+
+  // TODO: this is jank, make it better
+  handleOverlayClick(event) {
+    if (event.target.classList.contains('ReactModal__Overlay')) {
+      this.navigateHome();
+    }
   }
 
   navigateHome() {
@@ -33,31 +55,17 @@ class CustomModal extends Component {
     }
   }
 
-  componentDidMount() {
-    document.addEventListener('keydown', this.onEscape, false);
-  }
-
-  componentWillUnmount() {
-    document.removeEventListener('keydown', this.onEscape, false);
-  }
-
-  // TODO: make modal close on click outside
-  // overlayRef={(ref) => this.setState({overlayRef: ref})}
-  // componentDidUpdate() {
-  //   const {overlayRef} = this.state;
-  //   if (overlayRef) {
-  //     overlayRef.addEventListener('click', () => {});
-  //   }
-  // }
-
   render() {
     return (
       <Modal
         isOpen={true}
         style={modalStyle}
+        overlayRef={(ref) => this.setState({overlayRef: ref})}
       >
-        <Link to="/">x</Link>
-        {this.props.children}
+        <div>
+          <Link to="/">x</Link>
+          {this.props.children}
+        </div>
       </Modal>
     );
   }
