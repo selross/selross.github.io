10181a30df06dc343e730abf92196f884ff147f2
Jenny Heath <jennifer.w.heath@gmail.com>
2018-10-24 19:02:52 -0700

    label objects on hover

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,18 +9,24 @@ import Bookends from '../static/bookends.png';
 import Books from '../static/books.png';
 import BotanicalPoster from '../static/botanical-poster.png';
 import Envelope from '../static/envelope.png';
+import EnvelopeLabel from '../static/envelope-label.png';
 import FramedPhoto2 from '../static/framed-photo-2.png';
 import FramedPhoto3 from '../static/framed-photo-3.png';
 import FramedPhoto4 from '../static/framed-photo-4.png';
 import FramedPhoto5 from '../static/framed-photo-5.png';
 import Laptop from '../static/laptop.png';
+import LaptopLabel from '../static/laptop-label.png';
 import Plant from '../static/plant.png';
 import Plant2 from '../static/plant-2.png';
 import Plant3 from '../static/plant-3.png';
 import Plant4 from '../static/plant-4.png';
 import Polaroid from '../static/polaroid.png';
+import PolaroidLabel from '../static/polaroid-label.png';
 import Radio from '../static/radio.png';
+import RadioLabel from '../static/radio-label.png';
 import Tickets from '../static/tickets.png';
+
+// TODO: extract these styled components
 
 const Wrapper = styled.div``;
 
@@ -60,6 +66,13 @@ const PolaroidWrapper = styled(SelectableObjectWrapper)`
   width: 60px;
 `;
 
+const PolaroidLabelImage = styled.img`
+  width: 82px;
+  left: -13%;
+  top: 72%;
+  position: absolute;
+`;
+
 const FramedPhoto2Wrapper = styled(ObjectWrapper)`
   left: 59%;
   top: 94%;
@@ -90,7 +103,6 @@ const FramedPhoto3Wrapper = styled(ObjectWrapper)`
   width: 92px;
 `;
 
-
 const DeskObjectsWrapper = styled.div`
   position: absolute;
   width: 100%;
@@ -113,9 +125,16 @@ const BookendsWrapper = styled(ObjectWrapper)`
 `;
 
 const LaptopWrapper = styled(SelectableObjectWrapper)`
-  left: 40%;
+  left: 38%;
   top: 15%;
   width: 240px;
+`;
+
+const LaptopLabelImage = styled.img`
+  width: 90px;
+  left: 48%;
+  top: 23%;
+  position: absolute;
 `;
 
 const EnvelopeWrapper = styled(SelectableObjectWrapper)`
@@ -127,6 +146,13 @@ const EnvelopeWrapper = styled(SelectableObjectWrapper)`
   }
 `;
 
+const EnvelopeLabelImage = styled.img`
+  width: 90px;
+  left: -22%;
+  top: -21%;
+  position: absolute;
+`;
+
 const Plant2Wrapper = styled(ObjectWrapper)`
   left: 77%;
   top: 5%;
@@ -135,7 +161,7 @@ const Plant2Wrapper = styled(ObjectWrapper)`
 
 const PlantWrapper = styled(ObjectWrapper)`
   left: 18%;
-  bottom: 50%;
+  bottom: 55%;
   width: 230px;
 `;
 
@@ -152,16 +178,31 @@ const Plant4Wrapper = styled(ObjectWrapper)`
 `;
 
 const RadioWrapper = styled(SelectableObjectWrapper)`
-  left: 34%;
+  left: 30%;
   width: 120px;
-  bottom: 150px;
+  bottom: 50%;
+`;
+
+const RadioLabelImage = styled.img`
+  width: 90px;
+  left: 24%;
+  top: -28%;
+  position: absolute;
 `;
 
 class Home extends Component {
   constructor(props) {
     super(props);
-    this.state = {menuOpen: false};
+    this.state = {
+      envelopeHovered: false,
+      laptopHovered: false,
+      polaroidHovered: false,
+      radioHovered: false,
+      menuOpen: false
+    };
     this.handleClick = this.handleClick.bind(this);
+    this.handleHover = this.handleHover.bind(this);
+    this.handleUnhover = this.handleUnhover.bind(this);
   }
 
   // TODO: there is probably a better way of doing this
@@ -179,7 +220,16 @@ class Home extends Component {
     this.setState({menuOpen: false});
   }
 
+  handleHover(id) {
+    this.setState({[id]: true});
+  }
+
+  handleUnhover(id) {
+    this.setState({[id]: false});
+  }
+
   render() {
+    const {envelopeHovered, laptopHovered, polaroidHovered, radioHovered} = this.state;
     return (
       <Wrapper className="Home" onClick={this.handleClick}>
         <HamburgerMenu isOpen={this.state.menuOpen} />
@@ -187,7 +237,10 @@ class Home extends Component {
           <BotanicalPosterWrapper>
             <Image src={BotanicalPoster} />
           </BotanicalPosterWrapper>
-          <PolaroidWrapper>
+          <PolaroidWrapper
+            onMouseEnter={() => this.handleHover("polaroidHovered")}
+            onMouseLeave={() => this.handleUnhover("polaroidHovered")}>
+            {polaroidHovered && <PolaroidLabelImage src={PolaroidLabel} />}
             <Link to="/about">
               <Image src={Polaroid} />
             </Link>
@@ -207,7 +260,13 @@ class Home extends Component {
         </WallObjectsWrapper>
         <DeskObjectsWrapper>
           <Desk />
-          <RadioWrapper>
+          <PlantWrapper>
+            <Image src={Plant} />
+          </PlantWrapper>
+          <RadioWrapper
+            onMouseEnter={() => this.handleHover("radioHovered")}
+            onMouseLeave={() => this.handleUnhover("radioHovered")}>
+            {radioHovered && <RadioLabelImage src={RadioLabel} />}
             <Link to="/audio">
               <Image src={Radio} />
             </Link>
@@ -218,7 +277,10 @@ class Home extends Component {
           <Plant3Wrapper>
             <Image src={Plant3} />
           </Plant3Wrapper>
-          <EnvelopeWrapper>
+          <EnvelopeWrapper
+            onMouseEnter={() => this.handleHover("envelopeHovered")}
+            onMouseLeave={() => this.handleUnhover("envelopeHovered")}>
+            {envelopeHovered && <EnvelopeLabelImage src={EnvelopeLabel} />}
             <Link to="/contact">
               <Image src={Envelope} />
             </Link>
@@ -229,13 +291,13 @@ class Home extends Component {
           <Plant2Wrapper>
             <Image src={Plant2} />
           </Plant2Wrapper>
-          <PlantWrapper>
-            <Image src={Plant} />
-          </PlantWrapper>
-          <LaptopWrapper>
+          <LaptopWrapper
+            onMouseEnter={() => this.handleHover("laptopHovered")}
+            onMouseLeave={() => this.handleUnhover("laptopHovered")}>
             <Link to="/work">
               <Image src={Laptop} />
             </Link>
+            {laptopHovered && <LaptopLabelImage src={LaptopLabel} />}
           </LaptopWrapper>
         </DeskObjectsWrapper>
       </Wrapper>
