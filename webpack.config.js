9b60f8423c8ccf99cea3e3906532e0d75b21d56a
Jenny Heath <jennifer.w.heath@gmail.com>
2018-12-13 20:03:24 -0800

    change webpack config to point to the right directory in development

--- webpack.config.js
+++ webpack.config.js
@@ -34,7 +34,7 @@ module.exports = {
     filename: "bundle.js"
   },
   devServer: {
-    contentBase: path.join(__dirname, "public/"),
+    contentBase: path.join(__dirname, "/"),
     port: 3000,
     publicPath: "http://localhost:3000/dist/",
     historyApiFallback: true,
